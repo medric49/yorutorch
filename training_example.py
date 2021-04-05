89ff3d050a7090f13e70dfcb39b8ef4977881284
Medric Sonwa <medric49@gmail.com>
2021-04-05 14:41:45 +0200

    Update examples

--- training_example.py
+++ training_example.py
@@ -3,7 +3,7 @@ from torch import nn, optim
 import torchvision
 import string
 from efficientnet_pytorch import EfficientNet
-from . import datasets, transforms, data_modules, models, trainings
+from yorutorch import datasets, transforms, data_modules, models, trainings
 
 classes = string.ascii_lowercase + string.digits
 
